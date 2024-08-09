22afb7b6893a2939c98c626701f6fa077a5b5e7f
marcelakramer <101991467+marcelakramer@users.noreply.github.com>
2024-08-08 23:04:31 -0300

    fix: fix error in file after failed attempt of resolving conflicts

--- app/src/main/java/com/example/songhub/MainActivity.kt
+++ app/src/main/java/com/example/songhub/MainActivity.kt
@@ -7,16 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-<<<<<<< HEAD
 import com.example.songhub.ui.screens.LoginScreen
-=======
-import com.example.songhub.ui.screens.RegisterScreen
->>>>>>> d809a31 (refactor: making changes requested in the review)
 import com.example.songhub.ui.theme.SonghubTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,11 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             SonghubTheme {
                 Scaffold(modifier = Modifier.fillMaxSize(), containerColor = Color(0xFF040723)) { innerPadding ->
-<<<<<<< HEAD
                     LoginScreen(
-=======
-                    RegisterScreen(
->>>>>>> d809a31 (refactor: making changes requested in the review)
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -43,6 +34,5 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    SonghubTheme {
-    }
+    SonghubTheme {}
 }
