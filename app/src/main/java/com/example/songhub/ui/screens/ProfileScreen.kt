3ca82fca772bb39d00cb34f377d71494b73a3808
juanfarias <juanfariastk@gmail.com>
2024-08-30 01:13:25 -0300

    fix: name of screen and styling

---
+++ app/src/main/java/com/example/songhub/ui/screens/ProfileScreen.kt
@@ -0,0 +1,242 @@
+package com.example.songhub.ui.screens
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.songhub.R
+import com.example.songhub.model.User
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProfileScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController,
+) {
+    val user = User(
+        email = "Juanfariastk@gmail.com",
+        password = "12345",
+        username = "Juan Lindo"
+    )
+
+    if (user != null) {
+        Card(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(25.dp, 50.dp),
+            colors = CardDefaults.cardColors(containerColor = Color(0xFF060E43)),
+            shape = RoundedCornerShape(16.dp),
+        ) {
+            Column(
+                modifier = modifier
+                    .wrapContentSize()
+                    .background(Color(0xFF060e43))
+                    .padding(horizontal = 25.dp, vertical = 0.dp)
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    IconButton(
+                        onClick = { navController.navigate("main") },
+                        modifier = Modifier.padding(start = 0.dp)
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_back),
+                            contentDescription = "Back",
+                            tint = Color.White
+                        )
+                    }
+                }
+                Card(
+                    modifier = Modifier
+                        .size(200.dp)
+                        .padding(bottom = 4.dp),
+                    shape = RoundedCornerShape(100),
+                    colors = CardDefaults.cardColors(containerColor = Color(0xFFd9d9d9)),
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.camera_off_outline),
+                        contentDescription = "Profile Picture",
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(30.dp),
+                        tint = Color(0xFFc2c2c2)
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_upload),
+                        contentDescription = "Upload Icon",
+                        tint = Color.White,
+                        modifier = Modifier.size(20.dp)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = "Upload photo",
+                        color = Color.White,
+                        fontSize = 16.sp,
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                val username = remember { mutableStateOf(user.username) }
+                OutlinedTextField(
+                    value = username.value,
+                    onValueChange = { username.value = it },
+                    shape = MaterialTheme.shapes.medium,
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Color.Transparent,
+                        unfocusedBorderColor = Color.Transparent,
+                        containerColor = Color.White,
+                        focusedTextColor = Color.Black,
+                        unfocusedTextColor = Color.Black,
+                    ),
+                    placeholder = { Text("Username", color = Color(0xFF5A5A5A)) },
+                    label = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(55.dp)
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                val email = remember { mutableStateOf(user.email) }
+                OutlinedTextField(
+                    value = email.value,
+                    onValueChange = { email.value = it },
+                    shape = MaterialTheme.shapes.medium,
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Color.Transparent,
+                        unfocusedBorderColor = Color.Transparent,
+                        containerColor = Color.White,
+                        focusedTextColor = Color.Black,
+                        unfocusedTextColor = Color.Black,
+                    ),
+                    placeholder = { Text("Email", color = Color(0xFF5A5A5A)) },
+                    label = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(55.dp)
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                val password = remember { mutableStateOf("******") }
+                OutlinedTextField(
+                    value = password.value,
+                    onValueChange = { password.value = it },
+                    shape = MaterialTheme.shapes.medium,
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Color.Transparent,
+                        unfocusedBorderColor = Color.Transparent,
+                        containerColor = Color.White,
+                        focusedTextColor = Color.Black,
+                        unfocusedTextColor = Color.Black,
+                    ),
+                    placeholder = { Text("Password", color = Color(0xFF5A5A5A)) },
+                    label = null,
+                    visualTransformation = PasswordVisualTransformation(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(55.dp)
+                )
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    OutlinedButton(
+                        onClick = { /* Handle cancel */ },
+                        colors = ButtonDefaults.outlinedButtonColors(
+                            containerColor = Color.Transparent,
+                            contentColor = Color(0xFFEC5766)
+                        ),
+                        border = BorderStroke(2.dp, Color(0xFFEC5766)),
+                        shape = RoundedCornerShape(23.dp),
+                        modifier = Modifier
+                            .width(140.dp)
+                            .height(60.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = "Cancel Icon",
+                            tint = Color(0xFFEC5766),
+                            modifier = Modifier
+                                .size(32.dp)
+                                .padding(end = 9.dp)
+                        )
+                        Spacer(modifier = Modifier.width(4.dp))
+                        Text(text = "Cancel", color = Color(0xFFEC5766))
+                    }
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    OutlinedButton(
+                        onClick = { /* Handle save */ },
+                        colors = ButtonDefaults.outlinedButtonColors(
+                            containerColor = Color.Transparent,
+                            contentColor = Color(0xFF16A085)
+                        ),
+                        border = BorderStroke(2.dp, Color(0xFF16A085)),
+                        shape = RoundedCornerShape(23.dp),
+                        modifier = Modifier
+                            .width(140.dp)
+                            .height(60.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Check,
+                            contentDescription = "Save Icon",
+                            tint = Color(0xFF16A085),
+                            modifier = Modifier
+                                .size(32.dp)
+                                .padding(end = 9.dp)
+                        )
+                        Spacer(modifier = Modifier.width(4.dp))
+                        Text(text = "Save", color = Color(0xFF16A085))
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+            }
+        }
+    } else {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "No user logged in", fontSize = 36.sp, color = Color.Red)
+        }
+    }
+}
