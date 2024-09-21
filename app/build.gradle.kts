a0c7150e7317d53c0dbad4304b94c8f6ada50f24
MatheusSousa5068 <matheus5068trab@gmail.com>
2024-09-21 13:42:20 -0300

    adding implementations of build.grade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,5 +80,9 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.github.kittinunf.fuel:fuel:2.3.1") // Biblioteca Fuel
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
+
 
 }
