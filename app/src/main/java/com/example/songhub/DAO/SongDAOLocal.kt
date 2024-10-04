708c46e29d8a4d4a404fc9e89256c6e0e3b370e1
eztrela <pablolm.estrela@gmail.com>
2024-10-04 10:45:18 -0300

    fixing update song

--- app/src/main/java/com/example/songhub/DAO/SongDAOLocal.kt
+++ app/src/main/java/com/example/songhub/DAO/SongDAOLocal.kt
@@ -3,12 +3,16 @@ package com.example.songhub.DAO
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.songhub.model.Song
 
 @Dao
 interface SongDAOLocal {
     @Insert
     fun insert(song: Song)
+
+    @Update
+    fun update(song: Song)
 
     @Query("SELECT * FROM songs WHERE title = :title")
     fun getSongByTitle(title: String): Song?
